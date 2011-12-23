4df71cbc93c35abba432946ae850528ce441b32a
Nicholas C. Zakas <nicholas@nczconsulting.com>
2011-12-23 16:14:22 -0500

    prettier macro formats

---
+++ docs/api.js
@@ -0,0 +1,3 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {"classes":["parserlib.util.EventTarget","parserlib.util.StringReader","parserlib.util.SyntaxError","parserlib.util.SyntaxUnit","parserlib.util.TokenStreamBase"],"modules":[],"allModules":[]} };
+});
