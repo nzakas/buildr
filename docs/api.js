aee697bc48450ea30b17cc5b4e43f680e53f2d29
Nicholas C. Zakas <nicholas@nczconsulting.com>
2011-12-26 11:47:23 -0500

    added jsdoc task

--- docs/api.js
+++
@@ -1,3 +0,0 @@
-YUI.add("yuidoc-meta", function(Y) {
-   Y.YUIDoc = { meta: {"classes":["parserlib.util.EventTarget","parserlib.util.StringReader","parserlib.util.SyntaxError","parserlib.util.SyntaxUnit","parserlib.util.TokenStreamBase"],"modules":[],"allModules":[]} };
-});
